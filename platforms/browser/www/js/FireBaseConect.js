af19851a5c98118ca075162f41b3a13fa72fcf8b
toatzin Padilla <toatzin Padilla>
2016-03-15 16:15:13 -0600

    template Inicial...

---
+++ platforms/browser/www/js/FireBaseConect.js
@@ -0,0 +1,17 @@
+var glob = {};
+
+
+function firebaseGet(){
+  console.log("Inicio");
+  var myFirebaseRef = new Firebase("https://asphaltiot.firebaseio.com/");
+  myFirebaseRef.set({
+  'asphaltiot': {
+      Hola: "88123"
+  }
+  });
+}
+
+
+function hola(){
+  console.log("Hola");
+}
